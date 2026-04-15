8cf640f004d207411abe4916d89dc3abeb3ef21f
Divyarth <divyanshusaini@Dekshow-2.local>
2026-04-15 21:39:42 +0530

    Remove legacy webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    config.cache = false;
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
